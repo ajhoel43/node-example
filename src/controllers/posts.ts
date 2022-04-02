7a7082fa4d9e06b8e38786c5c00028f710743f4c
Ajhoel Tragedy <panduwiranata43@gmail.com>
2022-04-02 13:51:20 +0700

    Add Models  - init database connection  - add query exec function

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -1,6 +1,18 @@
 import { Request, Response } from "express";
+import { QueryOptions, format } from "mysql";
+import { queryExec } from "../models";
 
 export function getPost(req: Request, res: Response) {
-  console.log("Get Post Controller Works");
-  res.send("Get Post Controller Works");
+  let myQ: QueryOptions = {
+    sql: `SELECT id, username, name FROM ?? WHERE ?? LIKE ?`,
+    values: ['users', 'username', '%impor%'],
+  }
+
+  let user_data = queryExec(myQ).then((result) => {
+    console.log("User data", result);
+    res.send(result);
+  }).catch((err) => {
+    console.log("Query Fail", err);
+    res.send({ error: err.message });
+  });
 }
---
+++ src/models/index.ts
@@ -0,0 +1,57 @@
+import mysql, { MysqlError, ConnectionConfig, Connection, QueryOptions, format } from 'mysql';
+import dotenv from 'dotenv';
+
+interface dbConfig {
+  host: string,
+  port?: number,
+  user: string,
+  password: string,
+  database?: string
+}
+
+dotenv.config();
+
+const defaultConfig: ConnectionConfig = {
+  host: process.env.MYSQL_HOST,
+  port: 8010,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB
+};
+
+// const db = mysql.createConnection();
+export let db: Connection;
+
+export function connectDB(opt?: ConnectionConfig) {
+  let dbOpt = opt || defaultConfig;
+
+  db = mysql.createConnection(dbOpt);
+
+  db.connect((err: MysqlError) => {
+    if (err) {
+      console.log("MYSQL Error", err.message);
+      return;
+    } else {
+      console.log("MYSQL Connection Success");
+    }
+  });
+}
+
+export function disconnectDB() {
+  db.end();
+}
+
+export function queryExec(opts: QueryOptions) {
+  return new Promise((resolve, reject) => {
+    connectDB();
+
+    db.query(db.format(opts.sql, opts.values), (err, results) => {
+      if (err) throw err;
+
+      // console.log("Results: ", results);
+      resolve(results);
+    });
+
+    disconnectDB();
+  });
+}
